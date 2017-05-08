d7e67068430d06682a7d6a60c3d57c928b8ee8a4
Jacob Bieker <jacob@bieker.tech>
2017-05-08 08:39:00 +0200

    Start on test

---
+++ handin_2/test.py
@@ -0,0 +1,20 @@
+from McM_suffix_tree_build import built_tree
+from tandem_repeat_finder import find_tandem_repeats
+from util import print_tree, compare_tandem_repeats_result
+import sys, time
+import matplotlib.pyplot as plt
+
+"""
+
+Jacob Bieker 2017
+
+Testing the tandem_repeat code for O(nlogn) time
+
+
+"""
+
+# Create test data of all tandem repeats
+
+# Create test data of random inputs
+
+# Read in given data, get its length, and see how long it takes vs. size
